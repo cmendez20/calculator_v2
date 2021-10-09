6c18724d86dc3f51ebb92ca1f0e6edfca29c307a
Chris-Windows-Desktop <cmendez20@utexas.edu>
2021-10-09 00:40:15 -0500

    Add operate function and update display function

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 const inputScreen = document.querySelector('.input__num');
 const outputScreen = document.querySelector('.output__screen');
-const inputScreenNum = Number(
-  document.querySelector('.input__num').textContent
-);
 const calculator = document.querySelector('.calculator');
 let displayValue = 0;
 let numOne = 0;
 let numTwo = 0;
 let operations = false;
-let operationSymbol = '';
+let operationsCount = 0;
+let operator;
 
 const addNums = (numOne, numTwo) => numOne + numTwo;
 const subtractNums = (numOne, numTwo) => numOne - numTwo;
 const multiplyNums = (numOne, numTwo) => numOne * numTwo;
 const divideNums = (numOne, numTwo) => numOne / numTwo;
+
+const operate = (operator, numOne, numTwo) => {
+  switch (operator) {
+    case '+':
+      return addNums(numOne, numTwo);
+      break;
+    case '−':
+      return subtractNums(numOne, numTwo);
+      break;
+    case '×':
+      return multiplyNums(numOne, numTwo);
+      break;
+    case '÷':
+      return divideNums(numOne, numTwo);
+  }
+};
+
+const updateDisplay = () => {
+  outputScreen.textContent = `${numOne} ${operator} ${numTwo} =`;
+  inputScreen.textContent = operate(operator, numOne, numTwo);
+};
 
 calculator.addEventListener('click', e => {
   if (e.target.tagName === 'BUTTON' && Number(e.target.textContent) >= 0) {
@@ -32,8 +51,7 @@ calculator.addEventListener('click', e => {
     e.target.textContent !== '='
   ) {
     operations = true;
-    operationSymbol = e.target.textContent;
-    console.log(operationSymbol);
+    operator = e.target.textContent;
     numOne = Number(inputScreen.textContent);
     outputScreen.style.opacity = 1;
     outputScreen.textContent = `${numOne} ${e.target.textContent}`;
@@ -49,22 +67,8 @@ calculator.addEventListener('click', e => {
   }
 
   if (e.target.textContent == '=') {
-    console.log(operationSymbol);
     numTwo = Number(inputScreen.textContent);
-    console.log({ numOne }, { numTwo });
-    outputScreen.textContent = `${numOne} ${operationSymbol} ${numTwo} =`;
-    switch (operationSymbol) {
-      case '+':
-        inputScreen.textContent = `${addNums(numOne, numTwo)}`;
-        break;
-      case '−':
-        inputScreen.textContent = `${subtractNums(numOne, numTwo)}`;
-        break;
-      case '×':
-        inputScreen.textContent = `${multiplyNums(numOne, numTwo)}`;
-        break;
-      case '÷':
-        inputScreen.textContent = `${divideNums(numOne, numTwo)}`;
-    }
+    // console.log({ numOne }, { numTwo });
+    updateDisplay();
   }
 });
